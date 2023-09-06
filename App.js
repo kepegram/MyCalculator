10fa31db3f3366fbaab90721644ac0297baa36ac
Kadin Pegram <kpegra1@students.towson.edu>
2023-09-06 13:24:18 -0400

    Configuring eslinter

--- App.js
+++ App.js
@@ -1,139 +1,120 @@
 import React, {Component} from 'react';
-import { SafeAreaView, TouchableOpacity, Text, View } from 'react-native';
-import calculator, { initialTotal } from "./src/calculator";
-import styles from "./src/styles";
+import {SafeAreaView, TouchableOpacity, Text, View} from 'react-native';
+import calculator, {initialTotal} from './src/calculator';
+import styles from './src/styles';
 
 export default class App extends Component {
-    state = initialTotal;
+  state = initialTotal;
 
-    HandleInput = (type, value) => {
-        this.setState((state) => calculator(type, value, state));
-    };
+  HandleInput = (type, value) => {
+    this.setState(state => calculator(type, value, state));
+  };
 
-    render() {
-        return (
-            <SafeAreaView style={styles.container}>
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.calculatorTotal}>
+          <Text style={styles.totalSize}>
+            {parseFloat(this.state.currentValue).toLocaleString()}
+          </Text>
+        </View>
 
-                <View style={styles.calculatorTotal}>
-                    <Text style={styles.totalSize}>
-                        {parseFloat(this.state.currentValue).toLocaleString()}
-                    </Text>
-                </View>
-
-                <View style={styles.calculatorLayout}>
-                    <TouchableOpacity
-                        style={styles.clearButton}
-                        onPress={() => this.HandleInput('clear')}
-                    >
-                        <Text style={styles.textSize}>clear</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.signButton}
-                        onPress={() => this.HandleInput('operator', '/')}
-                    >
-                        <Text style={styles.textSize}>÷</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.calculatorLayout}>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', 7)}
-                    >
-                        <Text style={styles.textSize}>7</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', 8)}
-                    >
-                        <Text style={styles.textSize}>8</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', 9)}
-                    >
-                        <Text style={styles.textSize}>9</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.signButton}
-                        onPress={() => this.HandleInput('operator', 'x')}
-                    >
-                        <Text style={styles.textSize}>x</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.calculatorLayout}>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', 4)}
-                    >
-                        <Text style={styles.textSize}>4</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', 5)}
-                    >
-                        <Text style={styles.textSize}>5</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', 6)}
-                    >
-                        <Text style={styles.textSize}>6</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.signButton}
-                        onPress={() => this.HandleInput('operator', '-')}
-                    >
-                        <Text style={styles.textSize}>-</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.calculatorLayout}>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', 1)}
-                    >
-                        <Text style={styles.textSize}>1</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', 2)}
-                    >
-                        <Text style={styles.textSize}>2</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', 3)}
-                    >
-                        <Text style={styles.textSize}>3</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.signButton}
-                        onPress={() => this.HandleInput('operator', '+')}
-                    >
-                        <Text style={styles.textSize}>+</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.calculatorLayout}>
-                    <TouchableOpacity
-                        style={styles.zeroButton}
-                        onPress={() => this.HandleInput('number', 0)}
-                    >
-                        <Text style={styles.textSize}>0</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.numButton}
-                        onPress={() => this.HandleInput('number', '.')}
-                    >
-                        <Text style={styles.textSize}>.</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.signButton}
-                        onPress={() => this.HandleInput('equal', '=')}
-                    >
-                        <Text style={styles.textSize}>=</Text>
-                    </TouchableOpacity>
-                </View>
-
-            </SafeAreaView>
-        );
-    }
+        <View style={styles.calculatorLayout}>
+          <TouchableOpacity
+            style={styles.clearButton}
+            onPress={() => this.HandleInput('clear')}>
+            <Text style={styles.textSize}>clear</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.signButton}
+            onPress={() => this.HandleInput('operator', '/')}>
+            <Text style={styles.textSize}>÷</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.calculatorLayout}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', 7)}>
+            <Text style={styles.textSize}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', 8)}>
+            <Text style={styles.textSize}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', 9)}>
+            <Text style={styles.textSize}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.signButton}
+            onPress={() => this.HandleInput('operator', 'x')}>
+            <Text style={styles.textSize}>x</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.calculatorLayout}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', 4)}>
+            <Text style={styles.textSize}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', 5)}>
+            <Text style={styles.textSize}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', 6)}>
+            <Text style={styles.textSize}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.signButton}
+            onPress={() => this.HandleInput('operator', '-')}>
+            <Text style={styles.textSize}>-</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.calculatorLayout}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', 1)}>
+            <Text style={styles.textSize}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', 2)}>
+            <Text style={styles.textSize}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', 3)}>
+            <Text style={styles.textSize}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.signButton}
+            onPress={() => this.HandleInput('operator', '+')}>
+            <Text style={styles.textSize}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.calculatorLayout}>
+          <TouchableOpacity
+            style={styles.zeroButton}
+            onPress={() => this.HandleInput('number', 0)}>
+            <Text style={styles.textSize}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => this.HandleInput('number', '.')}>
+            <Text style={styles.textSize}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.signButton}
+            onPress={() => this.HandleInput('equal', '=')}>
+            <Text style={styles.textSize}>=</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
--- src/calculator.js
+++ src/calculator.js
@@ -1,68 +1,68 @@
 export const initialTotal = {
-    currentValue: '0',
-    operator: null,
-    previousValue: null
+  currentValue: '0',
+  operator: null,
+  previousValue: null,
 };
 
 export const handleNumber = (value, state) => {
-    if (state.currentValue === '0') {
-        return { currentValue: `${value}` };
-    }
+  if (state.currentValue === '0') {
+    return {currentValue: `${value}`};
+  }
 
-    return {
-        currentValue: `${state.currentValue}${value}`,
-    };
+  return {
+    currentValue: `${state.currentValue}${value}`,
+  };
 };
 
-const handleEqual = (state) => {
-    const { currentValue, previousValue, operator } = state;
+const handleEqual = state => {
+  const {currentValue, previousValue, operator} = state;
 
-    const current = parseFloat(currentValue);
-    const previous = parseFloat(previousValue);
-    const resetState = { operator: null, previousValue: null };
+  const current = parseFloat(currentValue);
+  const previous = parseFloat(previousValue);
+  const resetState = {operator: null, previousValue: null};
 
-    switch (operator) {
-        case '+':
-            return {
-                currentValue: `${previous + current}`,
-                ...resetState,
-            };
-        case '-':
-            return {
-                currentValue: `${previous - current}`,
-                ...resetState,
-            };
-        case 'x':
-            return {
-                currentValue: `${previous * current}`,
-            };
-        case '÷':
-            return {
-                currentValue: `${previous / current}`,
-                ...resetState,
-            };
-        default:
-            return state;
-    }
+  switch (operator) {
+    case '+':
+      return {
+        currentValue: `${previous + current}`,
+        ...resetState,
+      };
+    case '-':
+      return {
+        currentValue: `${previous - current}`,
+        ...resetState,
+      };
+    case 'x':
+      return {
+        currentValue: `${previous * current}`,
+      };
+    case '÷':
+      return {
+        currentValue: `${previous / current}`,
+        ...resetState,
+      };
+    default:
+      return state;
+  }
 };
 
 const calculator = (type, value, state) => {
-    switch (type) {
-        case 'number':
-            return handleNumber(value, state);
-        case 'clear':
-            return initialTotal;
-        case 'operator':
-            return {
-                operator: value,
-                previousValue: state.currentValue,
-                currentValue: '0',
-            };
-        case 'equal':
-            return handleEqual(state);
-        default:
-            return state;
-    }
+  switch (type) {
+    case 'number':
+      return handleNumber(value, state);
+    case 'clear':
+      return initialTotal;
+    case 'operator':
+      return {
+        operator: value,
+        previousValue: state.currentValue,
+        currentValue: '0',
+      };
+    case 'equal':
+      return handleEqual(state);
+    default:
+      return state;
+  }
 };
 
 export default calculator;
